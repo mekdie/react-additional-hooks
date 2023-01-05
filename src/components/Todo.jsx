f00a43f0475853a225249f36e407f8aa109d1169
McDony Lee <mcdonyleee@gmail.com>
2023-01-06 08:57:46 +1100

    ADded further hooks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,30 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from 'react'
 
 function Todo() {
-    const [loading, setLoading] = useState(true);
-    const [todo, setTodo] = useState({});
+  const [loading, setLoading] = useState(true)
+  const [todo, setTodo] = useState({})
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/todos/1")
-            .then((res) => res.json())
-            .then((data) => {
-                setTimeout(() => {
-                    setTodo(data);
-                    setLoading(false);
-                }, 3000);
-            });
-    }, []);
-    return loading ? <h3>Loading ...</h3> : <h1>{todo.title}</h1>;
+  const isMounted = useRef(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          if (isMounted.current) {
+            setTodo(data)
+            setLoading(false)
+          }
+        }, 3000)
+      })
+
+    // Runs when component is unmounted
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
+
+  return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>
 }
 
-export default Todo;
+export default Todo
---
+++ src/components/UseMemoExample1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
+const UseMemoExample1 = () => {
+    return <div>UseMemoExample1</div>;
+};
+
+export default UseMemoExample1;
